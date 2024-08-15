29b76b04e4aae521c4f667529d8da2ede4013cbc
Deborah Mendonça <deborahsoares01@gmail.com>
2024-08-14 22:13:08 -0300

    First commit

---
+++ typescript-curso-1-arquivos-iniciais/app/app.ts
@@ -0,0 +1,6 @@
+import { Negociacao } from "./models/negociacao.js";
+
+
+ const negociacao  = new Negociacao(10, new Date(), 1);
+
+ console.log(negociacao);
---
+++ typescript-curso-1-arquivos-iniciais/app/models/negociacao.ts
@@ -0,0 +1,12 @@
+export class Negociacao{
+    #valor;
+    #data;
+    #quantidade;
+
+
+    constructor(valor, data, quantidade){
+        this.#data = data;
+        this.#valor = valor;
+        this.#quantidade = quantidade;
+    }
+}
